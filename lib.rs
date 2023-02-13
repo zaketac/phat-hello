9ef55bdc023d3ce50b787b28059e6aad66beb459
zaketac <zaketac@gmail.com>
2023-02-13 10:20:17 +0100

    ciao

--- lib.rs
+++ lib.rs
@@ -5,7 +5,7 @@ extern crate alloc;
 use pink_extension as pink;
 
 #[pink::contract(env=PinkEnvironment)]
-mod phat_hello {
+mod phat_hello2 {
     use super::pink;
     use ink_prelude::{format, string::String};
     use pink::{http_get, PinkEnvironment};
@@ -35,8 +35,8 @@ mod phat_hello {
 
     #[derive(Deserialize, Encode, Clone, Debug, PartialEq)]
     pub struct EtherscanResponse<'a> {
-        status: &'a str,
-        message: &'a str,
+        jsonrpc: &'a str,
+        id: &'a str,
         result: &'a str,
     }
 
@@ -51,16 +51,16 @@ mod phat_hello {
         /// Such functions use the immutable reference `&self`
         /// so WILL NOT change the contract state.
         #[ink(message)]
-        pub fn get_eth_balance(&self, account: String) -> Result<String> {
-            if !account.starts_with("0x") && account.len() != 42 {
+        pub fn get_eth_balance(&self, block: String) -> Result<String> {
+            if !block.starts_with("0") && block.len() != 42 {
                 return Err(Error::InvalidEthAddress);
             }
 
-            // get account ETH balance with HTTP requests to Etherscan
+            // get info block with HTTP requests to Etherscan
             // you can send any HTTP requests in Query handler
             let resp = http_get!(format!(
-                "https://api.etherscan.io/api?module=account&action=balance&address={}",
-                account
+                "http://rpc-juno.mib.tech/block?height={}",
+                block
             ));
             if resp.status_code != 200 {
                 return Err(Error::HttpRequestFailed);
@@ -84,6 +84,7 @@ mod phat_hello {
         use ink_lang as ink;
 
         /// We test a simple use case of our contract.
+        /*
         #[ink::test]
         fn it_works() {
             // when your contract is really deployed, the Phala Worker will do the HTTP requests
@@ -91,12 +92,12 @@ mod phat_hello {
             pink_extension_runtime::mock_ext::mock_all_ext();
 
             let phat_hello = PhatHello::new();
-            let account = String::from("0xD0fE316B9f01A3b5fd6790F88C2D53739F80B464");
-            let res = phat_hello.get_eth_balance(account.clone());
+            let block = String::from("0xD0fE316B9f01A3b5fd6790F88C2D53739F80B464");
+            let res = phat_hello.get_eth_balance(block.clone());
             assert!(res.is_ok());
 
             // run with `cargo +nightly test -- --nocapture` to see the following output
-            println!("Account {} gets {} Wei", account, res.unwrap());
-        }
+            println!("Block {} gets {} Wei", block, res.unwrap());
+        }*/
     }
 }
